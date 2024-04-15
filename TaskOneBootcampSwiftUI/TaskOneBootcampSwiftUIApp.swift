8658478bbd6443e2a964b7c5896b26e321f343e7
acdominguez03 <acordondominguez31@gmail.com>
2024-04-15 22:48:09 +0200

    Initial Commit

---
+++ TaskOneBootcampSwiftUI/TaskOneBootcampSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskOneBootcampSwiftUIApp.swift
+//  TaskOneBootcampSwiftUI
+//
+//  Created by Andres Cordón on 15/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskOneBootcampSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
